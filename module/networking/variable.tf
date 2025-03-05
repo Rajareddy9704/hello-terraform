aa2487e828cf6e50ddc2daa1f70bc5c8fe41a031
Rajareddy9704 <157009326+Rajareddy9704@users.noreply.github.com>
2025-03-05 18:45:48 +0530

    Create variable.tf

---
+++ module/networking/variable.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "Hello-app-vpc"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_name" {
+  description = "Name of the public subnet"
+  type        = string
+  default     = "Hello-app-subnet"
+}
