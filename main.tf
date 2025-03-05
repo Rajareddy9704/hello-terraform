7b6694ccc6642a97addcbbacc54014f99c9db255
Rajareddy9704 <157009326+Rajareddy9704@users.noreply.github.com>
2025-03-05 18:47:02 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "networking" {
+  source      = "./modules/networking"
+  vpc_cidr    = "10.0.0.0/16"
+  vpc_name    = "Hello-app-vpc"
+  subnet_cidr = "10.0.1.0/24"
+  subnet_name = "Hello-app-subnet"
+}
