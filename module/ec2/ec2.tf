9edd0e7e3836bf99774f522e2ba5cee213abbbb8
Rajareddy9704 <157009326+Rajareddy9704@users.noreply.github.com>
2025-03-05 19:30:53 +0530

    Update ec2.tf

--- module/ec2/ec2.tf
+++ module/ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "tomcat" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.security_group]
+  vpc_security_group_ids = [aws_security_group.tomcat_sg.id]
   associate_public_ip_address = true
 
   user_data = file("${path.module}/install_tomcat.sh")  # Run script on startup
