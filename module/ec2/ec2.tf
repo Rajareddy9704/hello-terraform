89b0dd7f779d42e60cdc5876f949bdc3d76aaeca
Rajareddy9704 <157009326+Rajareddy9704@users.noreply.github.com>
2025-03-05 19:21:34 +0530

    Update ec2.tf

--- module/ec2/ec2.tf
+++ module/ec2/ec2.tf
@@ -12,3 +12,34 @@ resource "aws_instance" "tomcat" {
     Name = "Tomcat-Server"
   }
 }
+
+resource "aws_security_group" "tomcat_sg" {
+  name        = "tomcat-security-group"
+  description = "Allow SSH and Tomcat traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Tomcat-SG"
+  }
+}
+
