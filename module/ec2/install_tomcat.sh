e92942bc2e191aa48ee32fc041dfd994d0822368
Rajareddy9704 <157009326+Rajareddy9704@users.noreply.github.com>
2025-03-05 19:26:41 +0530

    Update install_tomcat.sh

--- module/ec2/install_tomcat.sh
+++ module/ec2/install_tomcat.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install -y openjdk-11-jdk
-
-# Install Tomcat
-TOMCAT_VERSION=9.0.80
-wget https://downloads.apache.org/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz
-sudo tar -xvzf apache-tomcat-$TOMCAT_VERSION.tar.gz -C /opt/
-sudo mv /opt/apache-tomcat-$TOMCAT_VERSION /opt/tomcat
-sudo chmod +x /opt/tomcat/bin/*.sh
-
-# Start Tomcat
-sudo /opt/tomcat/bin/startup.sh
+sudo yum update -y
+sudo yum install -y java-1.8.0-openjdk
+cd /opt
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.tar.gz
+tar xzvf apache-tomcat-9.0.71.tar.gz
+mv apache-tomcat-9.0.71 tomcat
+cd tomcat/bin
+chmod +x *.sh
+./startup.sh
