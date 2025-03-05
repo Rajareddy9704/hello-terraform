a387edaa0fb5de111abce98ee8ef6169afeec9bf
Rajareddy9704 <157009326+Rajareddy9704@users.noreply.github.com>
2025-03-05 19:09:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ module "networking" {
   subnet_cidr = "10.0.1.0/24"
   subnet_name = "Hello-app-subnet"
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "subnet_id" {
+  value = module.vpc.subnet_id
+}
